3a65686ef755f45025b5f49e063d4be0704eab83
woooyoung <axdsw121@gmail.com>
2026-03-27 16:43:37 +0900

    테마, 아이콘

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,27 @@
 'use client';
 
+import { ThemeProvider } from '@emotion/react';
 import Button from '@mui/material/Button';
+import { MdDeleteForever } from 'react-icons/md';
 
 export default function Home() {
   return (
     <>
       <button class="bg-red-500">버튼</button>
-      <Button variant="contained">안녕</Button>
+      {/* <ThemeProvider theme={theme}> */}
+      <Button
+        variant="contained"
+        onClick={() => {
+          if (confirm('삭제할거임?')) {
+            // 삭제 로직
+          } else {
+            // 삭제 안할 때 로직
+          }
+        }}>
+        삭제
+        <MdDeleteForever />
+      </Button>
+      {/* </ThemeProvider> */}
     </>
   );
 }
